3f647366baac8acb679e7b6f151c901b1afdf005
H <thisisafakeemail@lol.com>
2020-01-08 13:06:12 +0000

    first commit

--- src/main/java/com/qa/plate/core/PlateReg.java
+++ src/main/java/com/qa/plate/core/PlateReg.java
@@ -2,4 +2,15 @@ package com.qa.plate.core;
 
 public class PlateReg {
 
+	public int PlateReg (int input) {
+		if (input == 05) {
+			return 55;
+		}
+		return input;
+	}
+	
+	
+	
+	
+	
 }
--- src/test/java/com/qa/plate/core/PlateRegTest.java
+++ src/test/java/com/qa/plate/core/PlateRegTest.java
@@ -7,8 +7,13 @@ import org.junit.jupiter.api.Test;
 class PlateRegTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void plate_05_identify() {
+		PlateReg pr = new PlateReg();
+		int expected = 55;
+		int result = pr.PlateReg(05);
+		
+		assertEquals(expected, result);
+		
 	}
 
 }
