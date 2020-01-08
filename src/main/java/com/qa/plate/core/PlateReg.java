59394ca9914c65f25f484381c48cd1b2f414aef7
H <thisisafakeemail@lol.com>
2020-01-08 13:32:29 +0000

    first commit

--- src/main/java/com/qa/plate/core/PlateReg.java
+++ src/main/java/com/qa/plate/core/PlateReg.java
@@ -3,8 +3,14 @@ package com.qa.plate.core;
 public class PlateReg {
 
 	public int PlateReg (int input) {
+		if (input >=100) {
+			return 0;
+		}
+		if (input <= 0) {
+			return 0;
+		}
 		if (input>50) {
-			return (input-50)+1;
+			return (input-50);
 		}else {
 			return input+50;
 		}
--- src/test/java/com/qa/plate/core/PlateRegTest.java
+++ src/test/java/com/qa/plate/core/PlateRegTest.java
@@ -19,11 +19,44 @@ class PlateRegTest {
 	@Test
 	void plate_september_to_march_identify() {
 		PlateReg pr = new PlateReg();
-		int expected = 28;
+		int expected = 27;
 		int result = pr.PlateReg(77);
 		
 		assertEquals(expected, result);
 		
 	}
+	
+	@Test
+	void plate_identify_zero() {
+		PlateReg pr = new PlateReg();
+		int expected = 0;
+		int result = pr.PlateReg(0);
+		
+		assertEquals(expected, result);
+		
+	}
+	
+	@Test
+	void plate_identify_negative() {
+		PlateReg pr = new PlateReg();
+		int expected = 0;
+		int result = pr.PlateReg(-40);
+		
+		assertEquals(expected, result);
+		
+	}
+	
+	@Test
+	void plate_identify_over_max_value() {
+		PlateReg pr = new PlateReg();
+		int expected = 0;
+		int result = pr.PlateReg(100);
+		
+		assertEquals(expected, result);
+		
+	}
+	
+	
+	
 
 }
