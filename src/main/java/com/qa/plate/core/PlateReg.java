3829c6b6576318f0009088338951512872c06cce
H <thisisafakeemail@lol.com>
2020-01-08 13:19:16 +0000

    first commit

--- src/main/java/com/qa/plate/core/PlateReg.java
+++ src/main/java/com/qa/plate/core/PlateReg.java
@@ -3,8 +3,12 @@ package com.qa.plate.core;
 public class PlateReg {
 
 	public int PlateReg (int input) {
+		if (input>50) {
+			return (input-50)+1;
+		}else {
+			return input+50;
+		}
 		
-		return input+50;
 	}
 	
 	
--- src/test/java/com/qa/plate/core/PlateRegTest.java
+++ src/test/java/com/qa/plate/core/PlateRegTest.java
@@ -7,10 +7,20 @@ import org.junit.jupiter.api.Test;
 class PlateRegTest {
 
 	@Test
-	void plate_05_identify() {
+	void plate_march_to_september_identify() {
 		PlateReg pr = new PlateReg();
-		int expected = 55;
-		int result = pr.PlateReg(05);
+		int expected = 74;
+		int result = pr.PlateReg(24);
+		
+		assertEquals(expected, result);
+		
+	}
+	
+	@Test
+	void plate_september_to_march_identify() {
+		PlateReg pr = new PlateReg();
+		int expected = 28;
+		int result = pr.PlateReg(77);
 		
 		assertEquals(expected, result);
 		
