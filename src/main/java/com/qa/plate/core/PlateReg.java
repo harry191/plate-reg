b871e9cc1473a880a0e8f59ad42e4b46fd6ea06b
H <thisisafakeemail@lol.com>
2020-01-08 13:51:47 +0000

    first commit

--- src/main/java/com/qa/plate/core/PlateReg.java
+++ src/main/java/com/qa/plate/core/PlateReg.java
@@ -2,17 +2,24 @@ package com.qa.plate.core;
 
 public class PlateReg {
 
-	public int PlateReg (int input) {
+	public String PlateReg (String inputString) {
+		int input = Integer.parseInt(inputString);
+		
 		if (input >=100) {
-			return 0;
+			return "0";
 		}
 		if (input <= 0) {
-			return 0;
+			return "0";
 		}
 		if (input>50) {
-			return (input-50);
+			if ((input-50)<10) {
+				return ("0"+(input-50));
+			}else {
+				return (""+(input-50));
+			}
+			
 		}else {
-			return input+50;
+			return (""+(input+50));
 		}
 		
 	}
--- src/test/java/com/qa/plate/core/PlateRegTest.java
+++ src/test/java/com/qa/plate/core/PlateRegTest.java
@@ -9,8 +9,8 @@ class PlateRegTest {
 	@Test
 	void plate_march_to_september_identify() {
 		PlateReg pr = new PlateReg();
-		int expected = 74;
-		int result = pr.PlateReg(24);
+		String expected = "74";
+		String result = pr.PlateReg("24");
 		
 		assertEquals(expected, result);
 		
@@ -19,8 +19,8 @@ class PlateRegTest {
 	@Test
 	void plate_september_to_march_identify() {
 		PlateReg pr = new PlateReg();
-		int expected = 27;
-		int result = pr.PlateReg(77);
+		String expected = "27";
+		String result = pr.PlateReg("77");
 		
 		assertEquals(expected, result);
 		
@@ -29,8 +29,8 @@ class PlateRegTest {
 	@Test
 	void plate_identify_zero() {
 		PlateReg pr = new PlateReg();
-		int expected = 0;
-		int result = pr.PlateReg(0);
+		String expected = "0";
+		String result = pr.PlateReg("0");
 		
 		assertEquals(expected, result);
 		
@@ -39,8 +39,8 @@ class PlateRegTest {
 	@Test
 	void plate_identify_negative() {
 		PlateReg pr = new PlateReg();
-		int expected = 0;
-		int result = pr.PlateReg(-40);
+		String expected = "0";
+		String result = pr.PlateReg("-40");
 		
 		assertEquals(expected, result);
 		
@@ -49,12 +49,24 @@ class PlateRegTest {
 	@Test
 	void plate_identify_over_max_value() {
 		PlateReg pr = new PlateReg();
-		int expected = 0;
-		int result = pr.PlateReg(100);
+		String expected = "0";
+		String result = pr.PlateReg("100");
 		
 		assertEquals(expected, result);
 		
 	}
+	
+	@Test
+	void plate_identify_zero_infront_of_num_lower_than_ten() {
+		PlateReg pr = new PlateReg();
+		String expected = "08";
+		String result = pr.PlateReg("58");
+		
+		assertEquals(expected, result);
+		
+	}
+	
+
 	
 	
 	
