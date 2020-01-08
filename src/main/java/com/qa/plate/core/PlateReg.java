b4cf9aaa14dfd36a84688bb782c33f0faf04e2f8
H <thisisafakeemail@lol.com>
2020-01-08 13:12:41 +0000

    first commit

--- src/main/java/com/qa/plate/core/PlateReg.java
+++ src/main/java/com/qa/plate/core/PlateReg.java
@@ -3,10 +3,8 @@ package com.qa.plate.core;
 public class PlateReg {
 
 	public int PlateReg (int input) {
-		if (input == 05) {
-			return 55;
-		}
-		return input;
+		
+		return input+50;
 	}
 	
 	
